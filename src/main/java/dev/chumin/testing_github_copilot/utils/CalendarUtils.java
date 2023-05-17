4cfc5133fd9592905f1f98dbabb00fa1bf481d23
Oleg Chumin <tschumin.oleg@gmail.com>
2023-05-17 14:28:08 +0300

    Add new files

---
+++ src/main/java/dev/chumin/testing_github_copilot/utils/CalendarUtils.java
@@ -0,0 +1,17 @@
+package dev.chumin.testing_github_copilot.utils;
+
+import java.time.LocalDate;
+
+public class CalendarUtils {
+    int calculateDaysBetweenDates(LocalDate startDate, LocalDate endDate) {
+        return endDate.getDayOfYear() - startDate.getDayOfYear();
+    }
+
+    int calculateDaysBetweenDatesByCronoUnits(LocalDate startDate, LocalDate endDate) {
+        return (int) java.time.temporal.ChronoUnit.DAYS.between(startDate, endDate);
+    }
+
+    int calculateDaysBetweenDatesByPeriod(LocalDate startDate, LocalDate endDate) {
+        return java.time.Period.between(startDate, endDate).getDays();
+    }
+}
