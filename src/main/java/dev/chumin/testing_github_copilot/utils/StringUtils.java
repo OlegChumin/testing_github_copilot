b7b44d9cc305c5b30cf9a98ca227f09b5f3f3129
Oleg Chumin <tschumin.oleg@gmail.com>
2023-05-17 13:52:30 +0300

    Add new files

--- src/main/java/dev/chumin/testing_github_copilot/utils/StringUtils.java
+++ src/main/java/dev/chumin/testing_github_copilot/utils/StringUtils.java
@@ -6,6 +6,23 @@ public class StringUtils {
         return sb.reverse().toString();
     }
 
+    public static String isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return "Not anagrams";
+        }
+        int[] letters = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            letters[str1.charAt(i) - 'a']++;
+            letters[str2.charAt(i) - 'a']--;
+        }
+        for (int letter : letters) {
+            if (letter != 0) {
+                return "Not anagrams";
+            }
+        }
+        return "Anagrams";
+    }
+
     public static String reverseWords(String str) {
         String[] words = str.split(" ");
         StringBuilder sb = new StringBuilder();
